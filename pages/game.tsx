62cc9890960bdd574dea1d112989d751e5678f70
AlexPegg <alexpeggsailing@gmail.com>
2022-01-22 18:42:02 +0000

    basic grid and sort of camera

--- pages/game.tsx
+++ pages/game.tsx
@@ -1,46 +1,62 @@
-import React, { useRef, useState, Suspense } from 'react'
+import React, { useRef, useState, Suspense, useEffect } from 'react'
 import { Canvas, useFrame } from '@react-three/fiber'
+import { Sky, PointerLockControls } from "@react-three/drei"
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import * as THREE from "three"
+import { Vector3 } from 'three'
 
-const Box = props => {
-  const mesh = useRef()
 
-  const [hovered, setHover] = useState(false)
-  const [active, setActive] = useState(false)
+const GamePage = () => {
+    if (typeof window == 'undefined' || typeof document == 'undefined'){
+        return(
+            <div>Loading</div>
+        )
+    }
+    const scene = new THREE.Scene()
+    const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 )
 
-  useFrame(() => (mesh.current.rotation.x = mesh.current.rotation.y += 0.01))
+    const renderer = new THREE.WebGLRenderer()
+    renderer.setSize( window.innerWidth, window.innerHeight )
+    document.body.appendChild( renderer.domElement )
 
-  return (
-    <mesh
-      {...props}
-      ref={mesh}
-      scale={active ? [6, 6, 6] : [5, 5, 5]}
-      onClick={e => setActive(!active)}
-      onPointerOver={e => setHover(true)}
-      onPointerOut={e => setHover(false)}
-    >
-      <boxBufferGeometry attach="geometry" args={[1, 1, 1]} />
-      <meshStandardMaterial
-        attach="material"
-        color={hovered ? '#2b6c76' : '#720b23'}
-      />
-    </mesh>
-  )
+    const grid = new THREE.GridHelper( 20, 20 );
+    grid.rotateOnAxis(new THREE.Vector3(1,0,0), Math.PI / 2)
+    scene.add(grid)
+
+    const light = new THREE.AmbientLight()
+    light.position.set(0, 0, 5)
+    light.color.set('white')
+    scene.add(light)
+
+    const controls = new OrbitControls(camera, renderer.domElement)
+    camera.up.set( 0, 0, 1 );
+
+    camera.position.z = 5
+
+    function animate() {
+        requestAnimationFrame( animate )
+        controls.update()
+        //animation code
+
+        renderer.render( scene, camera )
+    };
+
+    animate()
+
+    window.addEventListener( 'resize', onWindowResize, false )
+
+    function onWindowResize(){
+
+        camera.aspect = window.innerWidth / window.innerHeight
+        camera.updateProjectionMatrix()
+        renderer.setSize( window.innerWidth, window.innerHeight )
+
+    }
+  
+    return (
+        <>
+        </>
+    )
 }
 
-const BirdsPage = () => {
-  return [
-    <h1>Click on me - Hover me :)</h1>,
-    <Canvas camera={{ position: [0, 0, 35] }}>
-      <ambientLight intensity={2} />
-      <pointLight position={[40, 40, 40]} />
-      <Suspense fallback={null}>
-        <Box position={[10, 0, 0]} />
-        <Box position={[-10, 0, 0]} />
-        <Box position={[0, 10, 0]} />
-        <Box position={[0, -10, 0]} />
-      </Suspense>
-    </Canvas>,
-  ]
-}
-
-export default BirdsPage
+export default GamePage
