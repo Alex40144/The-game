f151cd6b374427b93d47f2b9b95a387425d4ead9
AlexPegg <alexpeggsailing@gmail.com>
2022-05-11 20:54:19 +0100

    going to UE5 for game

--- pages/Play.tsx
+++ pages/Play.tsx
@@ -1,53 +1,7 @@
 import Head from 'next/head'
+import Link from 'next/link'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-
-import { Canvas, useFrame, useThree } from "@react-three/fiber"
-import { Physics, useSphere, useBox, usePlane } from "@react-three/cannon"
-
-function Ball({ args = [0.5, 32, 32] }) {
-  const { viewport } = useThree()
-  const [ref, api] = useSphere(() => ({ args: [0.5], mass: 1 }))
-  // Invisible plane, if hit it respawns the ball
-  usePlane(() => ({
-    position: [0, -viewport.height, 0],
-    rotation: [-Math.PI / 2, 0, 0],
-    onCollide: () => {
-      api.position.set(0, 0, 0)
-      api.velocity.set(0, 10, 0)
-    },
-  }))
-  return (
-    <mesh ref={ref}>
-      <sphereGeometry args={args} />
-      <meshStandardMaterial />
-    </mesh>
-  )
-}
-
-function Paddle({ args = [2, 0.5, 1] }) {
-  const [ref, api] = useBox(() => ({ args }))
-  useFrame((state) => {
-    api.position.set((state.mouse.x * state.viewport.width) / 2, -state.viewport.height / 2, 0)
-    api.rotation.set(0, 0, (state.mouse.x * Math.PI) / 5)
-  })
-  return (
-    <mesh ref={ref}>
-      <boxGeometry args={args} />
-      <meshStandardMaterial color="lightblue" />
-    </mesh>
-  )
-}
-
-function Enemy({ args = [2, 0.5, 1], color, ...props }) {
-  const [ref] = useBox(() => ({ args, ...props }))
-  return (
-    <mesh ref={ref}>
-      <boxGeometry args={args} />
-      <meshStandardMaterial color={color} />
-    </mesh>
-  )
-}
 
 export default function Learn() {
     return (
@@ -58,17 +12,30 @@ export default function Learn() {
                 <link rel="icon" href="/favicon.ico" />
             </Head>
 
-            <Canvas camera={{ position: [0, 5, 12], fov: 50 }}>
-                <ambientLight intensity={0.3} />
-                <pointLight position={[10, 10, 5]} />
-                <pointLight position={[-10, -10, -5]} />
-                <Physics gravity={[0, -30, 0]} defaultContactMaterial={{ restitution: 1.1 }}>
-                    <Ball />
-                    <Paddle />
-                    <Enemy color="orange" position={[2, 1, 0]} />
-                    <Enemy color="hotpink" position={[-2, 3, 0]} />
-                </Physics>
-            </Canvas>
+            <main className={styles.main}>
+                <h1 className={styles.title}>
+                    <Link href="https://github.com/Alex40144/The-game">
+                        <a>Download</a>
+                    </Link>
+                </h1>
+
+                <p className={styles.description}>
+                    The big mulitplayer community game
+                </p>
+          </main>
+
+          <footer className={styles.footer}>
+              <a
+                  href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+                  target="_blank"
+                  rel="noopener noreferrer"
+              >
+                  Powered by{' '}
+                  <span className={styles.logo}>
+                      <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+                  </span>
+              </a>
+          </footer>
       </div>
     )
 }
